500f205f24a2596045fd545a98bac5b4f561c966
Xorse6 <wisdomxorse928@gmail.com>
2024-12-14 18:44:49 +0000

    first commit

---
+++ server/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+import connectDB from "./configs/mongodb.js";
+
+
+// import connectDB from "./config/mongodb.js";
+// import authRouter from "./routes/authRoutes.js";
+// import userRouter from "./routes/userRoutes.js";
+
+const app = express();
+const port = process.env.PORT || 4000
+connectDB()
+
+// const allowedOrigins = ['http://localhost:5173']
+
+
+// Initialize middleware
+app.use(express.json())
+app.use(cors())
+
+
+// {origin: allowedOrigins, credentials: true}
+
+// API Endpoint
+app.get('/', (req,res)=> res.send("API Working"));
+// app.use('/api/auth', authRouter)
+// app.use('/api/user', userRouter)
+
+
+
+app.listen(port, ()=> console.log(`Server started on PORT: ${port}`));
